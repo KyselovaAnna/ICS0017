ce432e680f9e5d3d2d8e04f1ab0a2d52ee906f28
KyselovaAnna <a.g.kyselova@gmail.com>
2024-09-01 00:04:55 +0300

    Create Area of a Triangle.cpp

---
+++ Area of a Triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variables declared and initialized
+    int b = 7, h = 5;
+
+    // Finding area as float using typecasting
+    float area = (float)(b * h) / 2;
+
+    // Output the result
+    cout << "The area of the triangle is: " << area << endl;
+
+    return 0;
+}
