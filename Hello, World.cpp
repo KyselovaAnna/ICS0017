8793637f7a4f7278768b121adbe2881f61048c8d
KyselovaAnna <a.g.kyselova@gmail.com>
2024-09-01 00:02:25 +0300

    Create Hello, World.cpp

---
+++ Hello, World.cpp
@@ -0,0 +1,17 @@
+// Include the library for input and output operations
+#include <iostream>
+
+// Use the standard namespace to simplify code by omitting the std:: prefix
+using namespace std;
+
+// Define the main function of the program
+int main() {
+    // The first thing the program does is output the message "Hello, World!" to the screen
+    cout << "Hello, World!" << endl;
+    // Here we use the 'cout' object to output text to the screen
+    // 'endl' is a manipulator that moves the cursor to a new line after the text is displayed
+
+    // Return 0 to indicate the successful completion of the program
+    return 0;
+    // It's important to end the program by returning the value 0, as the 'main' function has a return type of int
+}
