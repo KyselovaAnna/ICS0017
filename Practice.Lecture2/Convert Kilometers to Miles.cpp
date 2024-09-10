8d00ee8f09b9390b282e3f90dbefabd17d4e0d62
KyselovaAnna <a.g.kyselova@gmail.com>
2024-09-10 16:53:07 +0300

    Create Convert Kilometers to Miles.cpp

---
+++ Practice.Lecture2/Convert Kilometers to Miles.cpp
@@ -0,0 +1,27 @@
+
+// Program to convert distance in kilometer to miles
+// The distance will be entered by the user.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // create variables for kilometers and miles
+    double km, miles;
+
+    // take input for kilometers
+    cout << "Enter distance in km: ";
+    cin >> km;
+
+    // conversion ratio of km to miles
+    double conversion_ratio = 0.621;
+
+    // computing distance in miles
+    miles = km * conversion_ratio;
+
+    // print the distance in miles
+    cout << "Distance in miles: " << miles;
+
+    return 0;
+}
