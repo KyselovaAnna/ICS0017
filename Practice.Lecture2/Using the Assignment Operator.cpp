247e290576efc90cdcfe7f7640995919db3221f5
KyselovaAnna <a.g.kyselova@gmail.com>
2024-09-10 11:49:59 +0300

    Create Using the Assignment Operator.cpp

---
+++ Practice.Lecture2/Using the Assignment Operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <tuple>
+using namespace std;
+
+void assignment_operator() {
+    
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    
+    int num1 {13};
+    int num2 {0};
+    
+    num1 = 5;
+    num2 = num1;
+   
+   //----WRITE YOUR CODE ABOVE THIS LINE----
+      
+   cout << num1 << " " << num2;
+}
